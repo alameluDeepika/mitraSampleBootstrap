63c1808ba981c61980f4bbf05a2e45024ca6f9e0
alameluDeepika <91462351+alameluDeepika@users.noreply.github.com>
2021-10-20 16:31:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function c(){
+    document.getElementById("side").style.display="none";
+    
+    document.getElementById("cone").style.display="none";
+    
+    
+}
+function o(){
+    
+    document.getElementById("side").style.display="block";
+    document.getElementById("cone").style.display="block";
+   
+    
+}
+function hide(){
+    document.getElementById("side").style.display="none";
+    
+    document.getElementById("cone").style.display="none";
+    
+    
+}
